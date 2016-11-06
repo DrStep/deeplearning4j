d6e2820df7844e73ad66dde6a904a88024bdbf8a
DrStep <drstep321@mail.ru>
2016-11-06 23:53:30 +0300

    Add test class, development begins

---
+++ deeplearning4j-scaleout/flink/dl4j-flink/src/main/java/api/TestClass.java
@@ -0,0 +1,13 @@
+package api;
+
+/**
+ * @author: Stepan Semiokhin
+ * E-mail: ssemiokhin@luxoft.com
+ * @since: 06.11.16
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
